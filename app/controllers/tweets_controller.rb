3d1270a2a0b3419831907da3887e0b7e4ff8481b
Michael Adams <madams6216@gmail.com>
2014-09-02 09:32:39 -0400

    changed new tweet redirect from show tweet to index page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.order("created_at desc")
   end
 
   # GET /tweets/1
@@ -29,7 +29,7 @@ class TweetsController < ApplicationController
     @tweet.user_id = current_user.id
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to "/tweets", notice: 'Tweet was successfully created.' }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
